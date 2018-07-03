dda9b2622f8ad2f127e610f1e66462828259ad6c
AroPho <40785016+AroPho@users.noreply.github.com>
2018-07-02 22:51:57 -0700

    Add files via upload

---
+++ CIS22B/Labs/Lab1.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+void getSize(int&);
+void getSpace(int&, array*);
+void inputData();
+void printData();
+void destory();
+
+int main()
+{
+    int num;
+    string
+    getSize(num);
+    cout << num;
+    getSpace();
+
+
+
+}
+
+void getSize(int &num)
+{
+    cout << "How many strings do you want?" << "\n";
+    cin >> num;
+}
+
+void getSpace(int &num)
