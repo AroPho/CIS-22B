6eb2ca6c3377722e2e5eb81e24343e134a3d498f
AroPho <aaronnguyen2009@yahoo.com>
2018-07-08 22:18:13 -0700

    LAB2

---
+++ Lab2.1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+struct Car
+{
+    string reportMark;
+    int CarNumber;
+    string kind;
+    bool loaded;
+    string destination;
+}
+
+
+int main()
+{
+
+
+}
