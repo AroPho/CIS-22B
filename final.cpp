1cdd03fba5a2e5f84147bf2a44519e95ecffc7ab
AroPho <aaronnguyen2009@yahoo.com>
2019-01-27 21:03:58 -0800

    Final commit

---
+++ final.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+    const int SIZE = 5;
+
+    double x[SIZE];
+
+    for(int i = 2; i<= SIZE; i++)
+
+    {
+
+            x[i] = 0.0;
+    }
+
+
+
+}
